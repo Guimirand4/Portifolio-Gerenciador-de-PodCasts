bca67ff38b643971e552f4f6c40124ec88a9caee
Guimirand4 <guilhermemiranda419@gmail.com>
2025-07-15 18:19:40 -0300

    feat: service criada

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,10 +1,11 @@
 import {IncomingMessage, ServerResponse} from 'http';
+import {serviceListEpisodes} from "../services/list-episodes-service"
 
 export const getListEpisodes = async (req : IncomingMessage, res: ServerResponse) => {
+
+    const content = await serviceListEpisodes();
+
     res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify(
-        {
-            name: "Guilherme",
-        })
+    res.end(JSON.stringify(content)
     );
 };
