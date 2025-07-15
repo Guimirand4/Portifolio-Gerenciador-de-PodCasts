61a400276b87d131ac8b138eb38f7228d4e946c8
Guimirand4 <guilhermemiranda419@gmail.com>
2025-07-14 23:49:04 -0300

    feat: criando server http com biblioteca ts

---
+++ docs/app.md
@@ -0,0 +1,5 @@
+#podcast manage
+
+Objetivo:
+um app no estilo netflix para centralizar diferentes episodios de podcasts
+separados por categoria
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import * as http from 'http';
+
+const port = process.env.PORT;
+
+const server = http.createServer((request: http.IncomingMessage , response:http.ServerResponse) => {
+
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
