35afa6ebb34dcb4d51818bb2eb3439e31d785e34
Guimirand4 <guilhermemiranda419@gmail.com>
2025-07-18 15:15:37 -0300

    feat: criacao app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,10 @@
 import * as http from 'http';
-import { getFilterEpisodes, getListEpisodes } from './controllers/podscasts-controller';
-import { Routes } from './routes/routes';
-import { HttpMethod } from './utils/http-methods';
+import { app } from './app';
+
 
 const port = process.env.PORT;
 
-const server = http.createServer(async (req: http.IncomingMessage , res:http.ServerResponse) => {
-    
-    const [baseUrl, queryString] = req.url?.split("?") ?? ["", ""];
-
-    if(req.method === HttpMethod.GET && baseUrl === Routes.LIST){
-       await getListEpisodes(req, res);
-
-    }
-
-    if(req.method === HttpMethod.GET && baseUrl === Routes.EPISODES){
-        await getFilterEpisodes(req, res);
-    }
-}
-);
+const server = http.createServer(app);
 
 
 server.listen(port, () => {
