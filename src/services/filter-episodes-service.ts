2310d73e1d1d00249ddc1683b16fe50c80d9621a
Guimirand4 <guilhermemiranda419@gmail.com>
2025-07-17 22:54:59 -0300

    feat: filtrando eps por nome

---
+++ src/services/filter-episodes-service.ts
@@ -0,0 +1,7 @@
+import { repositoryPodcasts } from "../repositories/podcasts-repository";
+
+
+export const serviceFilterEpisodes = async (podcastName: string) => {
+    const data = await repositoryPodcasts(podcastName);
+    return data;
+}
--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,7 +1,7 @@
 import { repositoryPodcasts } from "../repositories/podcasts-repository"
 
 export const serviceListEpisodes = async () => {
-    const data = await repositoryPodcasts;
+    const data = await repositoryPodcasts();
 
         return data;
 };
